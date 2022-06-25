ab34b522e8b5d0b46ef73bf26a1b9557564c8f63
mes-nz <mes-nz>
2022-06-25 23:02:42 +1200

    tidy up age checker

--- static/age_checker.js
+++ static/age_checker.js
@@ -3,34 +3,64 @@
 const yesButton = document.getElementById("over18");
 const noButton = document.getElementById("under18");
 
-checkSessionStorage()
+checkLocalStorage()
 
-yesButton.addEventListener('click', function(){
-    sessionStorage.setItem('ageChecked', true);
+
+function yesButtonClick() {
+    localStorage.setItem('ageChecked', true);
+    localStorage.setItem('lastAgeCheck', Date.now());
     // hide pop-up
     $("#ageCheckContainer").hide(2000)
     // show page content
     $("#dim-background").css("display", "none");
-    //allow scrolling
+    // re-allow scrolling;
     $("body").css("overflow-y", "visible");
-})
+}
 
-
-noButton.addEventListener('click', async function(){
+async function noButtonClick() {
+    // show warning
     $("#u18Warning").css("display", "block")
-    await sleep(5000)
+    // disable buttons
+    yesButton.removeEventListener("click", yesButtonClick);
+    noButton.removeEventListener("click", noButtonClick);
+    // change appearance of buttons
+    $(".over-18-button").addClass( "over-18-button-inactive" );
+    // redirect
+    await sleep(7000)
     window.location.assign('https://www.alcohol.org.nz/');
-})
+}
 
 
-// Check if we've already checked the user's age in this session.
-async function checkSessionStorage() {
+yesButton.addEventListener('click', yesButtonClick);
+noButton.addEventListener('click', noButtonClick);
+
+
+async function checkLocalStorage() {
     await sleep(1500)
-    if(!sessionStorage.getItem('ageChecked')) {
+    // Check if we've checked the user's age before.
+    if(!localStorage.getItem('ageChecked')) {
         checkAge();
+    }
+    // If we have, check how recent it was.
+    if (localStorage.getItem('ageChecked')) {
+        checkLastSession()
     }
 }
 
+
+function checkLastSession() {
+    // Check if it's been more than 1 hour since the last age check
+    var lastAgeCheck = localStorage.getItem('lastAgeCheck')
+    // console.log("last check: ", lastAgeCheck)
+    // console.log("current time: ", Date.now())
+    timeDelta = Date.now() - lastAgeCheck
+    // console.log("time delta: ", timeDelta)
+
+    // 1 hr = 3,600,000 milliseconds
+    if (timeDelta > 3600000) {
+        checkAge();
+    }
+}
 
 function checkAge() {
     // hide background
